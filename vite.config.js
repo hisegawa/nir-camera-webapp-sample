829c2b0674725480b234027f0fec21edc38229cc
hisegaw <kishunowa@gmail.com>
2025-02-02 19:00:05 +0900

    camera部の実装

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true,
+    https: true
+  },
+  plugins: [mkcert(), react()]
+});
